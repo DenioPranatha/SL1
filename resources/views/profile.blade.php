7f43577b7f19d37228e4fa756e5804de30037d9c
DenioPranatha <denioindonesia@gmail.com>
2023-03-31 22:40:10 +0700

    Menambahkan image organisasi dan skill

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -12,6 +12,7 @@
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Tinos&display=swap');
       </style>
+      <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <title>Denio's Web</title>
 </head>
 <body>
@@ -42,14 +43,13 @@
 
 
     <div class="jumbotron1 text-center">
-      <h1 class="display-4 smaller">About</h1>
-      <p class="lead">Energetic and inventive college student with superb problem-solving, communication, and analytical abilities. Seeking carreer advancement in computer science and engineering field. I master backend development programming languages (e.g. PHP and mySQL), data processing languages (e.g. C, python, RStudio, Tableau), and UI/UX design tools (e.g. CSS, Figma, Adobe Illustrator). Over the span of 5 years, I participated in science community, exchange study, and several invention festivals.</p>
+      <h1 class="display-4 smaller" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">About</h1>
+      <p class="lead" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Energetic and inventive college student with superb problem-solving, communication, and analytical abilities. Seeking carreer advancement in computer science and engineering field. I master backend development programming languages (e.g. PHP and mySQL), data processing languages (e.g. C, python, RStudio, Tableau), and UI/UX design tools (e.g. CSS, Figma, Adobe Illustrator). Over the span of 5 years, I participated in science community, exchange study, and several invention festivals.</p>
     </div>
 
     <div class="jumbotron jumbotron-fluid text-center">
       <div class="container">
-        <h1 class="display-4 smaller">Portfolio</h1>
-        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        <h1 class="display-4 smaller" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Portfolio</h1>
 
       </div>
     </div>
@@ -58,11 +58,11 @@
       <div class="carousel-inner">
         <div class="carousel-item">
 
-          <div class="card">
+          <div class="card h-100">
             <div class="wrapper">
               <img src="img/face.jpg" alt="...">
             </div>
-            <div class="card-body">
+            <div class="card-body text-center">
               <p class="card-text">Top 5</p>
               <p class="card-text">Young Innovation Festival BCA 2022</p>
             </div>
@@ -71,11 +71,11 @@
         </div>
         <div class="carousel-item">
 
-          <div class="card">
+          <div class="card h-100">
             <div class="wrapper">
               <img src="img/face.jpg" alt="...">
             </div>
-            <div class="card-body">
+            <div class="card-body text-center">
               <p class="card-text">Software Engineer</p>
               <p class="card-text">BLI Management Website 2022</p>
             </div>
@@ -83,11 +83,11 @@
         </div>
         <div class="carousel-item">
 
-          <div class="card">
+          <div class="card h-100">
             <div class="wrapper">
               <img src="img/face.jpg" alt="...">
             </div>
-            <div class="card-body">
+            <div class="card-body text-center">
               <p class="card-text">2nd Place</p>
               <p class="card-text">Graphic Design Promotion
                 Delaware State BPA 2020</p>
@@ -97,11 +97,11 @@
         </div>
         <div class="carousel-item">
 
-          <div class="card">
+          <div class="card h-100">
             <div class="wrapper">
               <img src="img/face.jpg" alt="...">
             </div>
-            <div class="card-body">
+            <div class="card-body text-center">
               <p class="card-text">Participant</p>
               <p class="card-text">Kennedy-Lugar Youth Exchange and Study (YES) Program 2019-2020</p>
             </div>
@@ -110,11 +110,11 @@
         </div>
         <div class="carousel-item">
 
-          <div class="card">
+          <div class="card h-100">
             <div class="wrapper">
               <img src="img/face.jpg" alt="...">
             </div>
-            <div class="card-body">
+            <div class="card-body text-center">
               <p class="card-text">Silver Medal</p>
               <p class="card-text">Indonesia International Invention Festival (13F) 2019</p>
             </div>
@@ -123,11 +123,11 @@
         </div>
         <div class="carousel-item">
 
-          <div class="card">
+          <div class="card h-100">
             <div class="wrapper">
               <img src="img/face.jpg" alt="...">
             </div>
-            <div class="card-body">
+            <div class="card-body text-center">
               <p class="card-text">3rd Place</p>
               <p class="card-text">TGC In Action Essay Competition IPB University 2018</p>
             </div>
@@ -136,11 +136,11 @@
         </div>
         <div class="carousel-item">
 
-          <div class="card">
+          <div class="card h-100">
             <div class="wrapper">
               <img src="img/face.jpg" alt="...">
             </div>
-            <div class="card-body">
+            <div class="card-body text-center">
               <p class="card-text">Class leader</p>
               <p class="card-text">The 2nd Term of PPTI 12 2021-2022</p>
             </div>
@@ -158,15 +158,50 @@
       </button>
     </div>
 
-    <div class="jumbotron3 text-center">
-      <h1 class="display-4 smaller">Organization Involvements</h1>
-      <p class="lead">The organizations in which I have participated</p>
+    <div class="jumbotron3 text-center justify-content-center">
+      <h1 class="display-4 smaller">Skills</h1>
+
+      <div class="bar-cont">
+        <div class="bar-capt">mySQL</div>
+        <div class="progress progress-striped active">
+          <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 93%;"></div>
+        </div>
+      </div>
+
+      <div class="bar-cont">
+        <div class="bar-capt">PHP</div>
+        <div class="progress progress-striped active">
+          <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 85%;"></div>
+        </div>
+      </div>
+
+      <div class="bar-cont">
+        <div class="bar-capt">HTML/CSS</div>
+        <div class="progress progress-striped active">
+          <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 82%;"></div>
+        </div>
+      </div>
+
+      <div class="bar-cont">
+        <div class="bar-capt">Python</div>
+        <div class="progress progress-striped active">
+          <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
+        </div>
+      </div>
     </div>
 
-    
+
+
     <div class="jumbotron4 text-center">
-      <h1 class="display-4 smaller">Social media</h1>
-      <p class="lead">...</p>
+      <h1 class="display-4 smaller">Organization Involvements</h1>
+      <p class="lead">The organizations in which I have participated</p>
+      <div class="org-container">
+        <img src="img/kirs.png" width="120px">
+        <img src="img/bpa.jpg" width="120px">
+        <img src="img/afs.png" width="120px">
+        <img src="img/ti12.png" width="120px">
+        <img src="img/pm.png" width="120px">
+      </div>
     </div>
 
     <div class="jumbotron2">
@@ -182,5 +217,9 @@
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
     </body>
 </html>
