0511e3c0ddaadfe6972560f9b1ee511f19df43a7
DenioPranatha <denioindonesia@gmail.com>
2023-04-01 21:21:10 +0700

    add social media

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -217,15 +217,28 @@
     </div>
 
     <div class="jumbotron2">
-      <h1 class="display-4 smaller" style="color:white">Denio Pranatha</h1>
-      <a class="lead" style="color:white">Contact</a><br>
-      <a class="lead" style="color:white">Web design</a><br>
-      <a class="lead" style="color:white">Logo design</a><br>
-      <a class="lead" style="color:white">Photography</a><br>
-      <a class="lead" style="color:white">Pencil sketch</a><br>
-      <a class="lead" style="color:white">Organization</a><br>
-      <p class="lead" style="color:white">© 2023 Denio Pranatha. All rights reserved.</p>
-    </div>
+      <h1 class="display-4 smaller display6" style="color:white">Denio Pranatha</h1>
+      <div class="btm-wrapper1">
+        <a class="lead" style="color:white; cursor: pointer;">Contact</a>
+        <a class="lead" style="color:white; cursor: pointer;">Web design</a>
+        <a class="lead" style="color:white; cursor: pointer;">Logo design</a>
+      </div>
+      <div class="btm-wrapper1">
+        <a class="lead" style="color:white; cursor: pointer;">Photography</a>
+        <a class="lead" style="color:white; cursor: pointer;">Database management</a>
+        <a class="lead" style="color:white; cursor: pointer;">Organization</a>
+      </div>
+      <div class="btm-wrapper2">
+        <a href="https://www.instagram.com/deniopranatha"><img src="img/ig.png" alt=""></a>
+        <a href="https://www.facebook.com/denniiopranathaa"><img src="img/fb.png" alt=""></a>
+        <a href="https://github.com/DenioPranatha"><img src="img/github.png" alt=""></a>
+      </div>
+      <div class="btm-wrapper2">
+        <div class="lead" style="color:white">deniopranatha</div>
+        <div class="lead" style="color:white">Denio Pranatha</div>
+        <div class="lead" style="color:white">DenioPranatha</div>
+      </div>
+      <p class="lead display6" style="color:white">© 2023 Denio Pranatha. All rights reserved.</p>
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
