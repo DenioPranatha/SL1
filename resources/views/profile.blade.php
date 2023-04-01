7d82f6f36602d706ac936d69b79c6ad8e14a7df5
DenioPranatha <denioindonesia@gmail.com>
2023-04-01 13:15:01 +0700

    Menambahkan foto, styling website

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -27,32 +27,37 @@
       <a class="nav-link" href="#">Services</a>
     </li>
   </ul>
-
+    <div class="blue blue1" data-aos="slide-right" data-aos-duration="1000"></div>
+    <div class="blue blue3" data-aos="slide-right" data-aos-duration="2000"></div>
     <div class="container">
+      <div class="blue blue2" data-aos="slide-left" data-aos-duration="1500"></div>
       <div class="row">
         <div class="im blc">
-          <img src="img/face.jpg" width="360px" class="center">
+          <img src="img/face.png" width="360px" class="center">
         </div>
         <div class="blc blc-text">
-          <h1 class="display-4">Denio Pranatha</h1>
-          <h3 class="bold lead mt-0">College Student | Graphic Designer</h3>
-          <h3 class="lead ent">"Whatever you are, be a good one"</h3>
+          <h1 class="display-4" data-aos="fade" data-aos-duration="1500" data-aos-delay="500">Denio Pranatha</h1>
+          <h3 class="bold lead mt-0" data-aos="fade" data-aos-duration="1500" data-aos-delay="500">College Student | Graphic Designer</h3>
+          <h3 class="lead ent" data-aos="fade" data-aos-duration="1000" data-aos-delay="1500">"Whatever you are, be a good one"</h3>
         </div>
       </div>
     </div>
 
 
     <div class="jumbotron1 text-center">
-      <h1 class="display-4 smaller" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">About</h1>
-      <p class="lead" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Energetic and inventive college student with superb problem-solving, communication, and analytical abilities. Seeking carreer advancement in computer science and engineering field. I master backend development programming languages (e.g. PHP and mySQL), data processing languages (e.g. C, python, RStudio, Tableau), and UI/UX design tools (e.g. CSS, Figma, Adobe Illustrator). Over the span of 5 years, I participated in science community, exchange study, and several invention festivals.</p>
+      <h1 class="display-4 smaller" data-aos="fade-up" data-aos-duration="1000">About</h1>
+      <p class="lead" data-aos="fade-up" data-aos-duration="1000">Energetic and inventive college student with superb problem-solving, communication, and analytical abilities. Seeking carreer advancement in computer science and engineering field. I master backend development programming languages (e.g. PHP and mySQL), data processing languages (e.g. C, python, RStudio, Tableau), and UI/UX design tools (e.g. CSS, Figma, Adobe Illustrator). Over the span of 5 years, I participated in science community, exchange study, and several invention festivals.</p>
     </div>
 
     <div class="jumbotron jumbotron-fluid text-center">
       <div class="container">
-        <h1 class="display-4 smaller" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Portfolio</h1>
+        <h1 class="display-4 smaller" data-aos="fade-up" data-aos-duration="1000">Portfolio</h1>
 
       </div>
     </div>
+
+    <div class="blue blue4" data-aos="slide-right" data-aos-duration="1000"></div>
+    <div class="blue blue5" data-aos="slide-left" data-aos-duration="1000"></div>
 
     <div id="carouselExample" class="carousel">
       <div class="carousel-inner">
@@ -60,10 +65,10 @@
 
           <div class="card h-100">
             <div class="wrapper">
-              <img src="img/face.jpg" alt="...">
+              <img src="img/ynfest.jpeg" alt="...">
             </div>
             <div class="card-body text-center">
-              <p class="card-text">Top 5</p>
+              <p class="card-text display5">Top 5</p>
               <p class="card-text">Young Innovation Festival BCA 2022</p>
             </div>
           </div>
@@ -73,10 +78,10 @@
 
           <div class="card h-100">
             <div class="wrapper">
-              <img src="img/face.jpg" alt="...">
+              <img src="img/se.jpg" alt="...">
             </div>
             <div class="card-body text-center">
-              <p class="card-text">Software Engineer</p>
+              <p class="card-text display5">Software Engineer</p>
               <p class="card-text">BLI Management Website 2022</p>
             </div>
           </div>
@@ -85,10 +90,23 @@
 
           <div class="card h-100">
             <div class="wrapper">
-              <img src="img/face.jpg" alt="...">
+              <img src="img/komti.jpg" alt="...">
             </div>
             <div class="card-body text-center">
-              <p class="card-text">2nd Place</p>
+              <p class="card-text display5">Class leader</p>
+              <p class="card-text">The 2nd Term of PPTI 12 2021-2022</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="carousel-item">
+
+          <div class="card h-100">
+            <div class="wrapper">
+              <img src="img/bpa-comp.jpg" alt="...">
+            </div>
+            <div class="card-body text-center">
+              <p class="card-text display5">2nd Place</p>
               <p class="card-text">Graphic Design Promotion
                 Delaware State BPA 2020</p>
             </div>
@@ -99,10 +117,10 @@
 
           <div class="card h-100">
             <div class="wrapper">
-              <img src="img/face.jpg" alt="...">
+              <img src="img/yes.jpeg" alt="...">
             </div>
             <div class="card-body text-center">
-              <p class="card-text">Participant</p>
+              <p class="card-text display5">Participant</p>
               <p class="card-text">Kennedy-Lugar Youth Exchange and Study (YES) Program 2019-2020</p>
             </div>
           </div>
@@ -112,10 +130,10 @@
 
           <div class="card h-100">
             <div class="wrapper">
-              <img src="img/face.jpg" alt="...">
+              <img src="img/i3f.jpeg" alt="...">
             </div>
             <div class="card-body text-center">
-              <p class="card-text">Silver Medal</p>
+              <p class="card-text display5">Silver Medal</p>
               <p class="card-text">Indonesia International Invention Festival (13F) 2019</p>
             </div>
           </div>
@@ -125,24 +143,11 @@
 
           <div class="card h-100">
             <div class="wrapper">
-              <img src="img/face.jpg" alt="...">
+              <img src="img/tgc.jpg" alt="...">
             </div>
             <div class="card-body text-center">
-              <p class="card-text">3rd Place</p>
+              <p class="card-text display5">3rd Place</p>
               <p class="card-text">TGC In Action Essay Competition IPB University 2018</p>
-            </div>
-          </div>
-
-        </div>
-        <div class="carousel-item">
-
-          <div class="card h-100">
-            <div class="wrapper">
-              <img src="img/face.jpg" alt="...">
-            </div>
-            <div class="card-body text-center">
-              <p class="card-text">Class leader</p>
-              <p class="card-text">The 2nd Term of PPTI 12 2021-2022</p>
             </div>
           </div>
 
@@ -159,7 +164,7 @@
     </div>
 
     <div class="jumbotron3 text-center justify-content-center">
-      <h1 class="display-4 smaller">Skills</h1>
+      <h1 class="display-4 smaller" data-aos="fade-up" data-aos-duration="1000">Skills</h1>
 
       <div class="bar-cont">
         <div class="bar-capt">mySQL</div>
@@ -196,22 +201,22 @@
       <h1 class="display-4 smaller">Organization Involvements</h1>
       <p class="lead">The organizations in which I have participated</p>
       <div class="org-container">
-        <img src="img/kirs.png" width="120px">
-        <img src="img/bpa.jpg" width="120px">
-        <img src="img/afs.png" width="120px">
-        <img src="img/ti12.png" width="120px">
-        <img src="img/pm.png" width="120px">
+        <img src="img/kirs.png" width="120px" data-aos="zoom-in" data-aos-duration="1000">
+        <img src="img/bpa.jpg" width="120px" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="400">
+        <img src="img/afs.png" width="120px" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="800">
+        <img src="img/ti12.png" width="120px" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="1200">
+        <img src="img/pm.png" width="120px" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="1600">
       </div>
     </div>
 
     <div class="jumbotron2">
       <h1 class="display-4 smaller" style="color:white">Denio Pranatha</h1>
-      <p class="lead" style="color:white">Contact</p>
-      <p class="lead" style="color:white">Web design</p>
-      <p class="lead" style="color:white">Logo design</p>
-      <p class="lead" style="color:white">Photography</p>
-      <p class="lead" style="color:white">Pencil sketch</p>
-      <p class="lead" style="color:white">Organization</p>
+      <a class="lead" style="color:white">Contact</a>
+      <a class="lead" style="color:white">Web design</a>
+      <a class="lead" style="color:white">Logo design</a>
+      <a class="lead" style="color:white">Photography</a>
+      <a class="lead" style="color:white">Pencil sketch</a>
+      <a class="lead" style="color:white">Organization</a>
       <p class="lead" style="color:white">© 2023 Denio Pranatha. All rights reserved.</p>
     </div>
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
